73fa94baa7b5cecdd4d248d7ad3bb25c82b39e9f
Jonas Lindenskov Nielsen <lindenskov.nielsen@gmail.com>
2020-02-06 17:39:37 +0100

    Added initial projects for flagtool (app and tests).

---
+++ MiniTwit.FlagTool.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiniTwit.FlagTool.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Flagtool!");
+        }
+    }
+}
