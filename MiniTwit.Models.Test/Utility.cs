e913d090f9839cb4a51b20cab4651c7c94fd84c4
Hjalte Dalland <hjda@itu.dk>
2020-02-13 13:34:28 +0100

    remove unused includes

--- MiniTwit.Models.Test/Utility.cs
+++ MiniTwit.Models.Test/Utility.cs
@@ -1,23 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
-using MiniTwit.Entities;
-using System;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using System.Text;
+﻿using MiniTwit.Entities;
 using System.Threading.Tasks;
-using Microsoft.Data.Sqlite;
 
 namespace MiniTwit.Models.Tests
 {
     static class Utility
     {
-        public static MiniTwitContext CreateMiniTwitContextContext([CallerMemberName] string testName = "", [CallerFilePath] string testNamePart2 = "")
-        {
-            var connection = new SqliteConnection("Datasource=:memory:");
-            connection.Open();
-            var options = new DbContextOptionsBuilder<MiniTwitContext>().UseSqlite(connection);
-            return new MiniTwitContext(options.Options);
-        }
+        
 
         public static async Task Add_dummy_data(UserRepository repository)
         {
