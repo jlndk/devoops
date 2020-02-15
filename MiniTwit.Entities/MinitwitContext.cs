37338fdc19c587516edbc4e110b97461aea3d971
Jakob Uttenthal Israelsen <jais@itu.dk>
2020-02-15 12:16:40 +0100

    Converted to IdentityDbContext

--- MiniTwit.Entities/MinitwitContext.cs
+++ MiniTwit.Entities/MinitwitContext.cs
@@ -1,10 +1,11 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace MiniTwit.Entities
 {
-    public class MiniTwitContext : DbContext, IMiniTwitContext
+    public class MiniTwitContext : IdentityDbContext<User, IdentityRole<int>, int>, IMiniTwitContext
     {
-        public DbSet<User> Users { get; set; }
         public DbSet<Message> Messages { get; set; }
 
         public MiniTwitContext(DbContextOptions<MiniTwitContext> options)
