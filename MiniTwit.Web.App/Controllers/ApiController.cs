509ffa9dea24a380e54697a3b47158b5dddf4e24
Hjalte Dalland <hjda@itu.dk>
2020-02-25 01:21:11 +0100

    it finally passes all the test

--- MiniTwit.Web.App/Controllers/ApiController.cs
+++ MiniTwit.Web.App/Controllers/ApiController.cs
@@ -14,14 +14,14 @@ namespace MiniTwit.Web.App.Controllers
 {
     public class ApiController : Controller
     {
-        
         private readonly ILogger<ApiController> _logger;
         private readonly IMessageRepository _messageRepository;
         private readonly IUserRepository _userRepository;
         private readonly UserManager<User> _userManager;
-        private static string _latest = "0";
+        private static int _latest = 0;
 
-        public ApiController(UserManager<User> userManager, ILogger<ApiController> logger, IMessageRepository messageRepository,
+        public ApiController(UserManager<User> userManager, ILogger<ApiController> logger,
+            IMessageRepository messageRepository,
             IUserRepository userRepository)
         {
             _userManager = userManager;
@@ -38,54 +38,67 @@ namespace MiniTwit.Web.App.Controllers
                 return (false, null);
             }
 
-            return (true, StatusCode(403, Json(new {status = 403, error_msg = "You are not authorized to use this resource!"})));
+            return (true,
+                StatusCode(403, Json(new {status = 403, error_msg = "You are not authorized to use this resource!"})));
         }
 
 
         public class LatestMessage
         {
-            public string Latest { get; set; }
+            public int? Latest { get; set; }
         }
+
         [Route("[controller]/latest/")]
         [HttpGet]
         [AllowAnonymous]
         public async Task<IActionResult> GetLatest()
         {
-            return Json(new {latest=_latest});
+            return Json(new {latest = _latest});
+        }
+
+        private void UpdateLatest(int? latestMessage)
+        {
+            if (latestMessage != null)
+            {
+                _latest = latestMessage.Value;
+            }
         }
 
         private void UpdateLatest(LatestMessage latestMessage)
         {
             if (latestMessage?.Latest != null)
             {
-                _latest = latestMessage.Latest;
+                _latest = latestMessage.Latest.Value;
             }
         }
-        
+
         [Route("[controller]/msgs/")]
         [HttpGet]
         [AllowAnonymous]
-        public async Task<IActionResult> GetMessages([FromBody] LatestMessage latestMessage, [FromQuery(Name = "no")] int number = 100)
+        public async Task<IActionResult> GetMessages([FromQuery(Name = "latest")] int? latestMessage,
+            [FromQuery(Name = "no")] int number = 100)
         {
             UpdateLatest(latestMessage);
-            var messages= (await _messageRepository.ReadAsync())
-                            .Take(number)
-                            .Select(m => new {content=m.Text, pub_date=m.PubDate, user=m.Author.UserName});
+            var messages = (await _messageRepository.ReadAsync())
+                .Take(number)
+                .Select(m => new {content = m.Text, pub_date = m.PubDate, user = m.Author.UserName});
             if (messages.Any())
                 return Json(messages);
             else
                 return StatusCode(204, Json(""));
         }
-        
-        [Route("[controller]/msgs/{id}")]
+
+        [Route("[controller]/msgs/{username}")]
         [HttpGet]
         [AllowAnonymous]
-        public async Task<IActionResult> UserGetMessages(int id, [FromBody] LatestMessage latestMessage, [FromQuery(Name = "no")] int number = 100)
+        public async Task<IActionResult> UserGetMessages(string username,
+            [FromQuery(Name = "latest")] int? latestMessage, [FromQuery(Name = "no")] int number = 100)
         {
             UpdateLatest(latestMessage);
-            var messages = (await _messageRepository.ReadAllMessagesFromUserAsync(id))
+            var user = await _userRepository.ReadAsyncByUsername(username);
+            var messages = (await _messageRepository.ReadAllMessagesFromUserAsync(user.Id))
                 .Take(number)
-                .Select(m => new {content=m.Text, pub_date=m.PubDate, user=m.Author.UserName});
+                .Select(m => new {content = m.Text, pub_date = m.PubDate, user = m.Author.UserName});
             return Json(messages);
         }
 
@@ -93,22 +106,26 @@ namespace MiniTwit.Web.App.Controllers
         public class MessagePost
         {
             public string Content { get; set; }
+            public int? Latest { get; set; }
         }
-        
-        [Route("[controller]/msgs/{id}")]
+
+        [Route("[controller]/msgs/{username}")]
         [HttpPost]
         [AllowAnonymous]
-        public async Task<IActionResult> UserPostMessages(int id, [FromBody] LatestMessage latestMessage, [FromBody] MessagePost messagePost)
+        public async Task<IActionResult> UserPostMessages(string username,
+            [FromQuery(Name = "latest")] int? latestMessage, [FromBody] MessagePost messagePost)
         {
             UpdateLatest(latestMessage);
-            var user = await _userRepository.ReadAsync(id);
+            UpdateLatest(messagePost.Latest);
+            var user = await _userRepository.ReadAsyncByUsername(username);
             if (user == null || messagePost?.Content == null)
             {
                 return NotFound();
             }
+
             var message = new Message
             {
-                Author =  user,
+                Author = user,
                 AuthorId = user.Id,
                 Flagged = 0,
                 PubDate = DateTime.Now,
@@ -123,35 +140,41 @@ namespace MiniTwit.Web.App.Controllers
             public string Username { get; set; }
             public string Email { get; set; }
             public string Pwd { get; set; }
-            
+            public int? Latest { get; set; }
         }
-        
+
         [Route("[controller]/register")]
         [HttpPost]
         [AllowAnonymous]
-        public async Task<IActionResult> RegisterUser([FromBody] LatestMessage latestMessage, [FromBody] RegisterPost registerPost)
+        public async Task<IActionResult> RegisterUser([FromBody] RegisterPost registerPost,
+            [FromQuery(Name = "latest")] int? latestMessage)
         {
             UpdateLatest(latestMessage);
+            UpdateLatest(registerPost.Latest);
             if (registerPost?.Username == null)
             {
                 return StatusCode(403,
                     Json(new {status = 400, error_msg = "You have to enter a username"}));
             }
+
             if (registerPost.Email == null || !registerPost.Email.Contains("@"))
             {
                 return StatusCode(403,
                     Json(new {status = 400, error_msg = "You have to enter a valid email address"}));
             }
+
             if (registerPost.Pwd == null)
             {
                 return StatusCode(403,
                     Json(new {status = 400, error_msg = "You have to enter a password"}));
             }
+
             if ((await _userRepository.ReadAsyncByUsername(registerPost.Username)) != null)
             {
                 return StatusCode(403,
                     Json(new {status = 400, error_msg = "The username is already taken"}));
             }
+
             var user = new User
             {
                 UserName = registerPost.Username,
@@ -161,32 +184,39 @@ namespace MiniTwit.Web.App.Controllers
             _logger.LogInformation(registerPost.Username + " created a new account with password.");
             return StatusCode(204, "");
         }
-        
-        [Route("[controller]/fllws/{id}")]
+
+        [Route("[controller]/fllws/{username}")]
         [HttpGet]
         [AllowAnonymous]
-        public async Task<IActionResult> UserGetFollows(int id, [FromBody] LatestMessage latestMessage, [FromQuery(Name = "no")] int number = 100)
+        public async Task<IActionResult> UserGetFollows(string username,
+            [FromQuery(Name = "latest")] int? latestMessage, [FromQuery(Name = "no")] int number = 100)
         {
             UpdateLatest(latestMessage);
-            var followers = (await _userRepository.GetFollows(id))?
+            var user = await _userRepository.ReadAsyncByUsername(username);
+            var followers = (await _userRepository.GetFollows(user.Id))?
                 .Take(number)
-                .Select(u => new {follows=u.UserName});
-            return Json(followers);
+                .Select(u => u.UserName);
+            return Json(new {follows = followers});
         }
 
         public class FollowPost
         {
             public string Follow { get; set; }
             public string UnFollow { get; set; }
+            public int? Latest { get; set; }
         }
-        [Route("[controller]/fllws/{id}")]
+
+        [Route("[controller]/fllws/{username}")]
         [HttpPost]
         [AllowAnonymous]
-        public async Task<IActionResult> UserFollow(int id, [FromBody] LatestMessage latestMessage, [FromBody] FollowPost followPost)
+        public async Task<IActionResult> UserFollow(string username, [FromQuery(Name = "latest")] int? latestMessage,
+            [FromBody] FollowPost followPost)
         {
             UpdateLatest(latestMessage);
+            UpdateLatest(followPost.Latest);
             if (followPost?.Follow != null)
             {
+                var follower = await _userRepository.ReadAsyncByUsername(username);
                 var followee = await _userRepository.ReadAsyncByUsername(followPost.Follow);
                 if (followee == null)
                 {
@@ -194,10 +224,11 @@ namespace MiniTwit.Web.App.Controllers
                     return StatusCode(404, "");
                 }
 
-                await _userRepository.AddFollowerAsync(followerId: id, followeeId: followee.Id);
+                await _userRepository.AddFollowerAsync(followerId: follower.Id, followeeId: followee.Id);
             }
             else if (followPost?.UnFollow != null)
             {
+                var follower = await _userRepository.ReadAsyncByUsername(username);
                 var followee = await _userRepository.ReadAsyncByUsername(followPost.UnFollow);
                 if (followee == null)
                 {
@@ -205,8 +236,9 @@ namespace MiniTwit.Web.App.Controllers
                     return StatusCode(404, "");
                 }
 
-                await _userRepository.RemoveFollowerAsync(followerId: id, followeeId: followee.Id);
+                await _userRepository.RemoveFollowerAsync(followerId: follower.Id, followeeId: followee.Id);
             }
+
             return StatusCode(204, "");
         }
     }
